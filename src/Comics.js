c85138d006ae07b72739a6860d37dc69f2846070
Maceo Lewis <maceo.code@gmail.com>
2018-11-04 06:32:12 -0500

    obtained character id

--- src/Comics.js
+++ src/Comics.js
@@ -11,24 +11,23 @@ class Comics extends React.Component {
   }
 
   componentDidMount() {
-    fetch('https://gateway.marvel.com:443/v1/public/characters/1009718/comics?apikey=117b458635106b9721749634b53fb07b')
+    fetch('https://gateway.marvel.com:443/v1/public/characters?name=wolverine&apikey=117b458635106b9721749634b53fb07b')
       .then(res => res.json())
-      .then(d => this.setState({ comics: d.data.results }))
-  }
+      .then(d => this.setState ({comics: d.data.results[0].id}));
+        
+      }
+  
 
-    render() {
-      return (
-      <div>
-        {this.state.comics.map((comic) => (
-            <div key={comic.id}>
-                  <h2>{comic.title}</h2>
-                  <p>{comic.description}</p>
-                  </div>
-       ))}
-      </div>
-      )
-  }
+  render() {
+    console.log(this.state.comics);
+    return (
+    <div>
+
+    </div>
+    )
 }
+}
+
 
 export default Comics;
 
